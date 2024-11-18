17a57254fa683c323b1a47a0615f103f888d00f5
Ayann12 <tantowi818@gmail.com>
2024-11-18 09:53:23 +0700

    ui login

--- lib/Views/login.dart
+++ lib/Views/login.dart
@@ -1,4 +1,7 @@
+import 'package:bcons/Components/button.dart';
 import 'package:bcons/Components/colors.dart';
+import 'package:bcons/Components/textfield.dart';
+import 'package:bcons/Views/signup.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -9,6 +12,12 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final userName = TextEditingController();
+  final password = TextEditingController();
+
+  bool isChecked = false;
+  bool isLoginTrue = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,11 +25,60 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "LOGIN",
+            const Text(
+              "E-BCons",
               style: TextStyle(color: primaryColor, fontSize: 40),
             ),
-            Image.asset("assets/logo.png"),
+            Image.asset(
+              "assets/logo.png",
+              width: 320,
+            ),
+            InputField(
+                hint: "Username",
+                icon: Icons.account_circle,
+                controller: userName),
+            InputField(
+                hint: "Password",
+                icon: Icons.lock,
+                controller: password,
+                passwordInvisible: true),
+            ListTile(
+              horizontalTitleGap: 2,
+              title: const Text("Remember me"),
+              leading: Checkbox(
+                activeColor: primaryColor,
+                value: isChecked,
+                onChanged: (value) {
+                  setState(() {
+                    isChecked = !isChecked;
+                  });
+                },
+              ),
+            ),
+            Button(label: "LOGIN", press: () {}),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Don't have an account?",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignupScreen()));
+                    },
+                    child: const Text("SIGN UP"))
+              ],
+            ),
+            isLoginTrue
+                ? Text(
+                    "Username or Password is incorrect",
+                    style: TextStyle(color: Colors.red.shade900),
+                  )
+                : const SizedBox(),
           ],
         ),
       ),
