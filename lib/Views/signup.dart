21145c2ff5538faa9aa3aaef8a30f13984ce8a96
Ayann12 <tantowi818@gmail.com>
2024-11-18 14:06:00 +0700

    ui register

--- lib/Views/signup.dart
+++ lib/Views/signup.dart
@@ -1,3 +1,7 @@
+import 'package:bcons/Components/button.dart';
+import 'package:bcons/Components/colors.dart';
+import 'package:bcons/Components/textfield.dart';
+import 'package:bcons/Views/login.dart';
 import 'package:flutter/material.dart';
 
 class SignupScreen extends StatefulWidget {
@@ -8,12 +12,73 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final fullname = TextEditingController();
+  final email = TextEditingController();
+  final userName = TextEditingController();
+  final password = TextEditingController();
+  final confirmPassword = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [],
+      body: Center(
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    "Register New Account",
+                    style: TextStyle(
+                        color: primaryColor,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                InputField(
+                    hint: "Full name",
+                    icon: Icons.person,
+                    controller: fullname),
+                InputField(hint: "Email", icon: Icons.email, controller: email),
+                InputField(
+                    hint: "Username",
+                    icon: Icons.account_circle,
+                    controller: userName),
+                InputField(
+                    hint: "Password", icon: Icons.lock, controller: password),
+                InputField(
+                    hint: "Re-enter password",
+                    icon: Icons.lock,
+                    controller: confirmPassword),
+                const SizedBox(
+                  height: 20,
+                ),
+                Button(label: "SIGN UP", press: () {}),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Already have an account?",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const LoginScreen()));
+                        },
+                        child: Text("LOGIN"))
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
